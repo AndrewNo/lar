3a0c1186ad2cb082c923f3a2f018e43c20a86633
Novakovsky Andrey <novakovsky@up9.cc>
2017-02-13 10:38:52 +0200

    some editions in css

--- resources/views/index/blog.blade.php
+++ resources/views/index/blog.blade.php
@@ -1,14 +1,14 @@
 @extends('index.base')
 
 @section('content')
-    <div class="fff"></div>
+
 
     <div class="content">
         @foreach($posts as $post)
             <div class="blog_posts">
                 <a href="/blog/{{ $post->id }}"><h1>{{ $post->title }}</h1></a>
                 <p>{!! $post->content !!}</p>
-                <p>{{  $post->created_at }}</p>
+                <p class="blog_posts_time">{{  $post->created_at }}</p>
             </div>
         @endforeach
     </div>
--- resources/views/index/contacts.blade.php
+++ resources/views/index/contacts.blade.php
@@ -3,26 +3,26 @@
 @section('content')
 
     <div class="content">
-        <div class="jumbotron">
-            <p>Send me message:</p>
+        <div class="contact_great">
+            <p>Отправьте мне сообщение:</p>
         </div>
-        <form action="/contact/store" method="post" class="form-group col-lg-6">
+        <form action="/contact/store" method="post" class="contact_form">
             {{ csrf_field() }}
             <div class="form-group">
-                <label for="name" class="control-label">Name:</label>
-                <input type="text" name="name" id="name" class="form-control">
+                <label for="name" class="contact_label">Имя:</label>
+                <input type="text" name="name" id="name" class="contact_input">
             </div>
 
             <div class="form-group">
-                <label for="email" class="control-label">E-mail:</label>
-                <input type="text" name="email" id="email" class="form-control">
+                <label for="email" class="contact_label">E-mail:</label>
+                <input type="text" name="email" id="email" class="contact_input">
             </div>
             <div class="form-group">
-                <label for="comment" class="control-label">Comment:</label>
-                <textarea name="comment" id="comment" cols="30" rows="10" class="form-control"></textarea>
+                <label for="comment" class="contact_label">Сообщение:</label>
+                <textarea name="comment" id="comment" cols="30" rows="10" class="contact_input"></textarea>
             </div>
             <div class="form-group">
-                <input type="submit" value="Send Message" class="btn btn-primary">
+                <input type="submit" value="Отправить сообщение" class="contact_btn">
             </div>
         </form>
     </div>
