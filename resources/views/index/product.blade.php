ebe4f9fce1a936db67e0f4e3519c7cee4e9110fb
Novakovsky Andrey <novakovsky@up9.cc>
2017-02-13 15:58:30 +0200

    css popup

--- resources/views/index/product.blade.php
+++ resources/views/index/product.blade.php
@@ -34,31 +34,32 @@
 
     <div class="clearfix"></div>
     <div class="order">
+        <div class="order_close">X</div>
         <form action="order-store" method="post" class="form-group col-lg-9">
             {{ csrf_field() }}
             <input type="hidden" name="product_id" value="{{ $product->id }}">
             <div class="form-group">
-                <label for="name" class="control-label">Name:</label>
-                <input type="text" name="name" id="name" class="form-control">
+                <label for="name" class="order_label">Имя:</label>
+                <input type="text" name="name" id="name" class="contact_input">
             </div>
             <div class="form-group">
-                <label for="phone" class="control-label">Phone:</label>
-                <input type="text" name="phone" id="phone" class="form-control">
+                <label for="phone" class="order_label">Телефон:</label>
+                <input type="text" name="phone" id="phone" class="contact_input">
             </div>
             <div class="form-group">
-                <label for="email" class="control-label">E-mail:</label>
-                <input type="text" name="email" id="email" class="form-control">
+                <label for="email" class="order_label">E-mail:</label>
+                <input type="text" name="email" id="email" class="contact_input">
             </div>
             <div class="form-group">
-                <label for="city" class="control-label">City:</label>
-                <input type="text" name="city" id="city" class="form-control">
+                <label for="city" class="order_label">Город:</label>
+                <input type="text" name="city" id="city" class="contact_input">
             </div>
             <div class="form-group">
-                <label for="comment" class="control-label">Comment:</label>
-                <textarea name="comment" id="comment" cols="30" rows="10" class="form-control"></textarea>
+                <label for="comment" class="order_label">Комментарий:</label>
+                <textarea name="comment" id="comment" cols="30" rows="10" class="contact_input"></textarea>
             </div>
             <div class="form-group">
-                <input type="submit" value="Order" class="btn btn-primary">
+                <input type="submit" value="Order" class="contact_btn">
             </div>
         </form>
     </div>
@@ -66,11 +67,29 @@
         $(document).ready(function () {
             $('#send').on('click', function (e) {
                 e.preventDefault();
+                document.body.scrollTop = 0;
                 $('.order').css({
                     'display': 'block', 'position': 'absolute', 'top': '20%', 'left': '30%', 'width': '900px'
                 });
                 $('body').css({'background-color': 'gray'});
+
             });
+
+            $('.order_close').on('click', function () {
+                $('.order').css({'display': 'none'});
+                $('body').css({'background-color': 'white'});
+            });
+            $(document).keydown(function(e) {
+
+                if( e.keyCode === 27 ) {
+
+                    $('.order').css({'display': 'none'});
+                    $('body').css({'background-color': 'white'});
+
+                }
+
+            });
+
         });
     </script>
 @stop
