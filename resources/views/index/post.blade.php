56383c048b5206e35e857210ed949d8e86674e9c
Novakovsky Andrey <novakovsky@up9.cc>
2017-02-22 16:47:38 +0200

    format date

--- resources/views/index/post.blade.php
+++ resources/views/index/post.blade.php
@@ -4,7 +4,8 @@
     <div class="content">
         <h1>{{$post->title}}</h1>
         {!! $post->content !!}
-        <p>{{ $post->created_at }}</p>
+        <p>{{ date('d/m/Y', strtotime($post->created_at)) }} <i>{{ date('H:i', strtotime($post->created_at)) }}</i></p>
+
     </div>
 
 @stop
